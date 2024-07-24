2a48a9db10b6438488d9d36fadb0d77d37ac3a81
Miqueias Rodrigues Barreto <miqueiasr560@gmail.com>
2024-07-24 17:00:04 -0300

    adad

--- carrinho/src/routes/carrinho/carrinho.js
+++ carrinho/src/routes/carrinho/carrinho.js
@@ -3,7 +3,7 @@ const router_carrinho = express.Router()
 const data = require("../../database/config.js")
 
 router_carrinho.get("/listar", (req, res) => {
-    data.query("select * from carrinho",(error, dados)=> {
+    data.query("select foto.fotos1,titulo.nometitulo,titulo.autor,carrinho.quantidade,preco.precoatual,titulo.idtitulo,carrinho.total from saraivalivrodb.fotos foto inner join saraivalivrodb.titulos titulo on foto.idfotos=titulo.idfoto inner join saraivacarrinhodb.carrinho carrinho on titulo.idtitulo=carrinho.idprodutoinner join saraivalivrodb.precos preco on preco.idpreco = titulo.idpreco where carrinho.idusuario=?;",(error, dados)=> {
         if(error) {
             res.status(500).send({msg: "Erro ao carregar os dados"})
         } else {
@@ -11,5 +11,27 @@ router_carrinho.get("/listar", (req, res) => {
         }
     })
 })
+
+router_carrinho.get("/listar/:id", (req, res) => {
+    data.query("select foto.fotos1,titulo.nometitulo,titulo.autor,carrinho.quantidade,preco.precoatual,titulo.idtitulo,carrinho.total from saraivalivrodb.fotos foto inner join saraivalivrodb.titulos titulo on foto.idfotos=titulo.idfoto inner join saraivacarrinhodb.carrinho carrinho on titulo.idtitulo=carrinho.idprodutoinner join saraivalivrodb.precos preco on preco.idpreco = titulo.idpreco where carrinho.idusuario=1;",req.params.id, (error, dados) => {
+        if(error) {
+            res.status(500).send({msg: "Erro ao carregar os dados"})
+        } else {
+            res.status(200).send({msg: "Ok", payload:dados})
+        }
+    })
+})
+
+router_carrinho.post("/inserir", (req, res) => {
+    const dados = req.body
+    data.query("insert into carrinho set ?", [dados], (error, dados) => {
+        if(error) {
+            res.status(500).send({msg: "Erro ao colocar no carrinho"})
+        } else {
+            res.status(200).send({msg: "Ok", payload:dados})
+        }
+    })
+})
+
 
 module.exports = router_carrinho
